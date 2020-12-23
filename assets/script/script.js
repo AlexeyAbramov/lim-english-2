df661e8b8c4ea832241f11b207e7941b531c1651
AlexeyAbramov <abramov.alexey.93@ya.ru>
2020-12-23 22:21:42 +0300

    saved-1

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,13 @@
 // $(document).ready(function(){
 //     $('#raiting-tabs').tabs()
 // })
-$(document).ready(function(){
-    $('.leaderboard-raiting__item').click(function(){
-        $('.leaderboard-raiting__item').removeClass('active');
-        $(this).addClass('active');
-    })
+$(document).ready(function () {
+  $(".leaderboard-raiting__item").click(function () {
+    $(".leaderboard-raiting__item").removeClass("active");
+    $(this).addClass("active");
+  });
+  $(".filter-course__link").click(function () {
+    $(".filter-course__link").removeClass("filter__button_active");
+    $(this).addClass("filter__button_active");
+  });
 });
