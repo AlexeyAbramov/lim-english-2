b32d4ccefab548ebf0e16858f53ed49ae886658c
AlexeyAbramov <abramov.alexey.93@ya.ru>
2020-12-27 02:51:26 +0300

    t1

--- assets/script/script.js
+++ assets/script/script.js
@@ -141,5 +141,5 @@ $(document).ready(function () {
 //   progressBar(percent);
 // });
 });
-let test =document.querySelector('.svg-progress__circle_load').style;
-console.log(test.strokeDashoffset = 100);
+let test = document.querySelector('.svg-progress__circle_load').style.strokeDashoffset = +250;
+console.log(test);
